4bbe26b83a90877f6a4f4a10efe71b1ba069c575
Ignacio Mengochea <142363950+IgnacioMengochea@users.noreply.github.com>
2024-10-20 20:03:45 -0300

    Add files via upload

--- db_films.sql
+++ db_films.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 26-09-2023 a las 07:46:42
--- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.0.28
+-- Tiempo de generación: 20-10-2024 a las 23:23:39
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,100 +24,128 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `films`
+-- Estructura de tabla para la tabla `peliculas`
 --
 
-CREATE TABLE `films` (
-  `ID_FILMS` int(11) NOT NULL,
-  `Nombre_Film` varchar(50) NOT NULL,
-  `Director_film` varchar(30) NOT NULL,
-  `puntuacion_user` varchar(20) NOT NULL,
-  `Fecha_estreno` date NOT NULL,
-  `Id_puntuacion` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `peliculas` (
+  `id_peliculas` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `director` varchar(50) NOT NULL,
+  `fecha_estreno` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
 
 --
--- Volcado de datos para la tabla `films`
+-- Volcado de datos para la tabla `peliculas`
 --
 
-INSERT INTO `films` (`ID_FILMS`, `Nombre_Film`, `Director_film`, `puntuacion_user`, `Fecha_estreno`, `Id_puntuacion`) VALUES
-(1, 'Taxi Driver', 'Martin Scorsese', '10/10', '1977-03-24', 1),
-(2, 'Twelve monkeys', 'Terry Gilliam', '9.6/10', '1996-01-05', 2),
-(3, 'Scarface', 'Brian De Palma', '9.5/10', '1984-02-09', 3),
-(4, 'The Truman Show', 'Peter Weir', '9.5/10', '1998-10-22', 4);
+INSERT INTO `peliculas` (`id_peliculas`, `nombre`, `director`, `fecha_estreno`) VALUES
+(6, 'Batman', 'Lautaro', '2024-10-23'),
+(8, 'Superman', 'Juan', '2024-10-23'),
+(9, 'Iron man', 'Facundo', '2024-10-23'),
+(10, 'El hombre araña', 'Rodrigo', '2024-11-07'),
+(11, 'Hulk', 'Lautaro', '2024-11-14'),
+(12, 'deadpool', 'Lautaro', '2024-11-20');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `puntuacion_paginas`
+-- Estructura de tabla para la tabla `puntuaciones`
 --
 
-CREATE TABLE `puntuacion_paginas` (
-  `codpuntuacion` int(11) NOT NULL,
-  `Nombre_Film` varchar(30) NOT NULL,
-  `IMDb` varchar(20) NOT NULL,
-  `Filmaffinity` varchar(20) NOT NULL,
-  `Sensacine` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `puntuaciones` (
+  `id_puntuaciones` int(11) NOT NULL,
+  `valoracion` decimal(3,1) NOT NULL,
+  `empresa` varchar(20) NOT NULL,
+  `id_pelicula` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
 
 --
--- Volcado de datos para la tabla `puntuacion_paginas`
+-- Volcado de datos para la tabla `puntuaciones`
 --
 
-INSERT INTO `puntuacion_paginas` (`codpuntuacion`, `Nombre_Film`, `IMDb`, `Filmaffinity`, `Sensacine`) VALUES
-(1, 'Taxi Driver', '8.2/10', '8.1/10', '4.5/5'),
-(2, 'Twelve monkeys', '8/10', '7.4/10', '3.8/5'),
-(3, 'Scarface', '8.3/10', '8.2/10', '4.5/5'),
-(4, 'The Truman Show', '8.2/10', '7.7/10', '4.3/5');
+INSERT INTO `puntuaciones` (`id_puntuaciones`, `valoracion`, `empresa`, `id_pelicula`) VALUES
+(10, 4.0, 'Imdb', 6),
+(17, 5.0, 'SensaCine', 12),
+(18, 5.0, 'SensaCine', 9),
+(19, 2.0, 'Imdb', 11),
+(20, 3.0, 'Filmaffinity', 11),
+(21, 10.0, 'Filmaffinity', 8),
+(22, 9.0, 'Imdb', 9),
+(23, 7.0, 'Filmaffinity', 10);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id_usuario` int(11) NOT NULL,
+  `usuario` varchar(100) NOT NULL,
+  `password` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`id_usuario`, `usuario`, `password`) VALUES
+(1, 'webadmin', '$2y$10$IYn0jLwmKQUE5icQEzatv./P5VdzCL37MbxvhA6d94IAF76HYD7jK');
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `films`
+-- Indices de la tabla `peliculas`
 --
-ALTER TABLE `films`
-  ADD PRIMARY KEY (`ID_FILMS`),
-  ADD KEY `puntuacion-pagina` (`Id_puntuacion`);
+ALTER TABLE `peliculas`
+  ADD PRIMARY KEY (`id_peliculas`);
 
 --
--- Indices de la tabla `puntuacion_paginas`
+-- Indices de la tabla `puntuaciones`
 --
-ALTER TABLE `puntuacion_paginas`
-  ADD PRIMARY KEY (`codpuntuacion`);
+ALTER TABLE `puntuaciones`
+  ADD PRIMARY KEY (`id_puntuaciones`),
+  ADD KEY `FKpelicula` (`id_pelicula`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_usuario`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT de la tabla `films`
+-- AUTO_INCREMENT de la tabla `peliculas`
 --
-ALTER TABLE `films`
-  MODIFY `ID_FILMS` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+ALTER TABLE `peliculas`
+  MODIFY `id_peliculas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
--- AUTO_INCREMENT de la tabla `puntuacion_paginas`
+-- AUTO_INCREMENT de la tabla `puntuaciones`
 --
-ALTER TABLE `puntuacion_paginas`
-  MODIFY `codpuntuacion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+ALTER TABLE `puntuaciones`
+  MODIFY `id_puntuaciones` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
--- Filtros para la tabla `films`
+-- Filtros para la tabla `puntuaciones`
 --
-ALTER TABLE `films`
-  ADD CONSTRAINT `puntuacion-pagina` FOREIGN KEY (`Id_puntuacion`) REFERENCES `puntuacion_paginas` (`codpuntuacion`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Filtros para la tabla `puntuacion_paginas`
---
-ALTER TABLE `puntuacion_paginas`
-  ADD CONSTRAINT `puntuacion_paginas_ibfk_1` FOREIGN KEY (`codpuntuacion`) REFERENCES `films` (`Id_puntuacion`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `puntuaciones`
+  ADD CONSTRAINT `puntuaciones_ibfk_1` FOREIGN KEY (`id_pelicula`) REFERENCES `peliculas` (`id_peliculas`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

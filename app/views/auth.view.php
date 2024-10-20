4de27b56e1401489e101e97a9c024f58ec7f89e4
Ignacio Mengochea <142363950+IgnacioMengochea@users.noreply.github.com>
2024-10-20 20:13:35 -0300

    Update auth.view.php

--- app/views/auth.view.php
+++ app/views/auth.view.php
@@ -6,9 +6,4 @@ class AuthView {
     public function showLogin($error = '') {
         require 'templates/form_login.phtml';
     }
-/*
-    public function showSignup($error = '') {
-        require 'templates/form_signup.phtml';
-    }
-*/
 }
